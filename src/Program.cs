64168371908ed82783ad90ff4ed916456ed29706
Dreamcatcher- Trices <dreamcatcher647@hotmail.com>
2017-08-15 20:04:13 +0200

    initial commit

--- src/Program.cs
+++ src/Program.cs
@@ -9,7 +9,7 @@
     {
         static void Main()
         {
-            var hostname = "deep-moon-1452";
+            var hostname = "evening-badlands-37733";
 
             var port = System.Environment.GetEnvironmentVariable("PORT");
             var nancyHost = new NancyHost(new Uri("http://" + hostname + ".herokuapp.com:" + port), new Uri("http://0.0.0.0:" + port));
